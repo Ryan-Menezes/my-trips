af81f6287a3a0bee9c5ad2d3b7cd997a0cbf9bdf
Ryan de Menezes <menezesryan1010@gmail.com>
2023-04-20 14:17:03 -0300

    Configuring the PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: isProd,
+});
+
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  reactStrictMode: true,
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>My Trips</title>
         <link rel="shortcut icon" href="/img/ico-512.png" />
         <link rel="apple-touch-icon" href="/img/ico-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with with TypeScript, React, NextJS and Styled Components"
