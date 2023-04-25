83d29b243bf6d44878bd211e3dd0a108be07dc3d
Ryan de Menezes <menezesryan1010@gmail.com>
2023-04-25 12:02:53 -0300

    Add leaflet map

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/ico-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        />
         <meta
           name="description"
           content="A simple project to show my favorite spots in the world."
